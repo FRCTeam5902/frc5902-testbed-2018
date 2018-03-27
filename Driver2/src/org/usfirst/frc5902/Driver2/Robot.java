61a038e0cb023a910d2e6a456d884df0542a984e
FRC5902 <FRC5902@10.100.8.16>
2018-03-27 16:03:44 -0400

    Patch - 4.5.1

--- Driver2/src/org/usfirst/frc5902/Driver2/Robot.java
+++ Driver2/src/org/usfirst/frc5902/Driver2/Robot.java
@@ -123,7 +123,7 @@ public class Robot extends TimedRobot {
 		  {
 			System.out.println("Left Auto");
 			autonomousCommand = new autoPos1L();
-		  } else {
+		  } else if (Robot.gameData.charAt(0) == 'R'){
 			System.out.println("Right Auto");
 			autonomousCommand = new autoPos1R();
 		  }
@@ -137,9 +137,9 @@ public class Robot extends TimedRobot {
     		  {
     			System.out.println("Left Auto");
     			autonomousCommand = new autoPos3L();
-    		  } else {
+    		  } else if (Robot.gameData.charAt(0) == 'R') {
     			System.out.println("Right Auto");
-    			autonomousCommand = new autoPos3R();
+    			autonomousCommand = new autoPos1();
     		  }
     		   System.out.println("Compiling if statment");
         }
